d2b218fe3e1afd2201e19f878e3e06f3191f897d
Pouriya <pouriya.jamshidi@gmail.com>
2022-09-26 19:08:39 +0200

    format plotter

--- anagram/plotter.py
+++ anagram/plotter.py
@@ -13,7 +13,7 @@ commands = {
     batch["command"]: [sum(batch["times"]) / len(batch["times"])] for batch in results
 }
 
-sorted_results = {k: v for k, v in sorted(commands.items(), key=lambda item: item[1])}
+sorted_results = dict(sorted(commands.items(), key=lambda item: item[1]))
 sorted_key_list = list(sorted_results)
 
 # X axis position
@@ -34,11 +34,51 @@ v_median = commands["V"]
 plt.subplots(figsize=(15, 10))
 
 # Make the plot
-plt.bar(rust_position, rust_median, color="r", width=BAR_WIDTH, edgecolor="grey", label="Rust", align="center")
-plt.bar(go_position, go_median, color="c", width=BAR_WIDTH, edgecolor="grey", label="Go", align="center")
-plt.bar(python_position, python_median, color="b", width=BAR_WIDTH, edgecolor="grey", label="Python", align="center")
-plt.bar(nim_position, nim_median, color="y", width=BAR_WIDTH, edgecolor="grey", label="Nim", align="center")
-plt.bar(v_position, v_median, color="k", width=BAR_WIDTH, edgecolor="grey", label="Nim", align="center")
+plt.bar(
+    rust_position,
+    rust_median,
+    color="r",
+    width=BAR_WIDTH,
+    edgecolor="grey",
+    label="Rust",
+    align="center",
+)
+plt.bar(
+    go_position,
+    go_median,
+    color="c",
+    width=BAR_WIDTH,
+    edgecolor="grey",
+    label="Go",
+    align="center",
+)
+plt.bar(
+    python_position,
+    python_median,
+    color="b",
+    width=BAR_WIDTH,
+    edgecolor="grey",
+    label="Python",
+    align="center",
+)
+plt.bar(
+    nim_position,
+    nim_median,
+    color="y",
+    width=BAR_WIDTH,
+    edgecolor="grey",
+    label="Nim",
+    align="center",
+)
+plt.bar(
+    v_position,
+    v_median,
+    color="k",
+    width=BAR_WIDTH,
+    edgecolor="grey",
+    label="V",
+    align="center",
+)
 
 plt.xlabel("Programming Language", fontweight="bold", fontsize=15)
 plt.ylabel("Time (s)", fontweight="bold", fontsize=15)
